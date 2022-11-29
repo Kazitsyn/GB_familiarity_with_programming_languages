9d1183c29aa9204de183e97ea047b371d0cd8485
Kazitsyn <71144819+Kazitsyn@users.noreply.github.com>
2022-11-29 20:30:12 +0300

    add q1

---
+++ Seminar_1/Q1/Program.cs
@@ -0,0 +1,8 @@
+﻿//Напишите программу, которая на выход принимает число и выдает его квадрат
+Console.WriteLine("Введите число");
+string numberStr = Console.ReadLine() ?? "";
+int number = Convert.ToInt32(numberStr);
+
+int result = number * number;
+
+Console.WriteLine($"Квадрат числа {number} = {result}");
