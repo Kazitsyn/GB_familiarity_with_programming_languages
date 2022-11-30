bec1f9d12c198daf2d4ee540870498c2c5a17511
Kazitsyn <71144819+Kazitsyn@users.noreply.github.com>
2022-11-30 19:48:35 +0300

    add L2E010

---
+++ Lecture_2/Example010_MethodArray/Program.cs
@@ -0,0 +1,14 @@
+﻿int [] array = {11,221,41,4,61,17,18,19,55};
+
+int n = array.Length;
+int find = 4;
+int index = 0;
+
+while (index < n){
+    if (array[index] == find){
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+
+}
