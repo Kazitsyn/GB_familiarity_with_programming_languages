4fd03365f9d921fbf7d2f1237388fd555b8d9106
Kazitsyn <71144819+Kazitsyn@users.noreply.github.com>
2022-12-04 15:50:03 +0300

    add s2q2

---
+++ Seminar_2/S2Q2/Program.cs
@@ -0,0 +1,21 @@
+﻿/* Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа. */
+
+int GetRandomNumber()
+{
+    return new Random().Next(100, 1000); // 100 ... 1000
+}
+
+void ShowRandomNumberString(int number)
+{
+    Console.WriteLine($"Случайное число: {number}");
+    string str = number.ToString();
+    Console.WriteLine($"> {str[0]}{str[2]}");
+}
+ShowRandomNumberString(GetRandomNumber());
+
+int ShowRandomNumber(int number)
+{
+    Console.WriteLine($"Случайное число: {number}");
+    return ((number / 100)*10)+(number % 10);
+}
+Console.WriteLine(ShowRandomNumber(GetRandomNumber()));
