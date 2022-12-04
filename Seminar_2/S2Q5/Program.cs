dcfe1c7829152e03ab4a0538c2ebc7c473cd422b
Kazitsyn <71144819+Kazitsyn@users.noreply.github.com>
2022-12-04 17:03:22 +0300

    add s2q5

---
+++ Seminar_2/S2Q5/Program.cs
@@ -0,0 +1,13 @@
+﻿/* Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого*/
+Console.WriteLine("Введите первое число: ");
+int number0 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+bool SquareNumber (int number0, int number1)
+{
+    if (number0 * number0 == number1 || number1 * number1 == number0)return true;
+    return false;
+}
+
+Console.WriteLine(SquareNumber(number0,number1));
