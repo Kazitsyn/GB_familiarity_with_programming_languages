f07f215cebfd333c15ef6d1d4514cd9dfcd174fa
Kazitsyn <71144819+Kazitsyn@users.noreply.github.com>
2022-12-04 16:35:45 +0300

    add s2q4

---
+++ Seminar_2/S2Q4/Program.cs
@@ -0,0 +1,10 @@
+﻿/* Напишите программу, которая принимает на вход число и проверяет, кратное оно 7 и 23 */
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+void Multiple7And23(int number)
+{
+    if (number % 7 == 0 && number % 23 == 0) Console.WriteLine("Чило кратное 7 и 23");
+    else  Console.WriteLine("Чило не кратное 7 и 23");
+}
+Multiple7And23(number);
