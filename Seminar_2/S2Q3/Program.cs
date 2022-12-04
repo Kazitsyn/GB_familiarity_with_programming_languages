a9fe9bfe4b067e82cab107df0aaf99309fa74c1f
Kazitsyn <71144819+Kazitsyn@users.noreply.github.com>
2022-12-04 16:17:09 +0300

    add s2q3

---
+++ Seminar_2/S2Q3/Program.cs
@@ -0,0 +1,9 @@
+﻿/* Напишите программу, которая будет принимать на вход два числа и выводить является ли второе число кратное первому. 
+Если не является то выводить остаток от деления */
+void Multiple(int number0, int number1)
+{
+    int result = number0 % number1;
+    if (result == 0) Console.WriteLine("Число 1 кратное числу 2");
+    else Console.WriteLine(result);
+}
+Multiple(555, 5);
