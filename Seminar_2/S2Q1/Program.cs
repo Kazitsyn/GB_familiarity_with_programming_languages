f9be1e08f5766de6dcf80ea644e6ba6f59abc410
Kazitsyn <71144819+Kazitsyn@users.noreply.github.com>
2022-12-04 14:53:59 +0300

    add seminar_2

---
+++ Seminar_2/S2Q1/Program.cs
@@ -0,0 +1,30 @@
+﻿/* Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа. */
+
+// возвращает рандомное число из отрезка [left, right]
+int GetRandomNumber(int left, int right)
+{
+    Random rnd = new Random();
+    int number = rnd.Next(left, right + 1); //[left, right]
+    return number;
+}
+
+
+//возвращает максимальную цифру числа
+int GetMaxDigitOfNumber (int number)
+{
+    int firstDigit = number / 10;
+    int secondDigit = number % 10;
+
+    if(firstDigit > secondDigit)
+        return firstDigit;
+    else
+        return secondDigit;
+}
+
+int number = GetRandomNumber(10, 99);
+
+Console.WriteLine($"сгенерированное число: {number}");
+
+int maxDigit = GetMaxDigitOfNumber(number);
+
+Console.WriteLine($"наибольшая цифра числа: {maxDigit}");
