9d1ec203294a024116f09c77e33eccbe3f99fb3e
Kazitsyn <71144819+Kazitsyn@users.noreply.github.com>
2022-12-20 20:12:24 +0300

    add seminar 7

---
+++ Seminar_7/S7Q2/Program.cs
@@ -0,0 +1,61 @@
+﻿/*
+Задача 48: Задайте двумерный массив размера m на n, каждый элемент в массиве находится по формуле: Aₘₙ = m+n. Выведите полученный массив на экран.
+m = 3, n = 4.
+0 1 2 3
+1 2 3 4
+2 3 4 5
+*/
+
+int GetNumber(string message)
+{
+    int result = 0;
+
+    while(true)
+    {
+        Console.WriteLine(message);
+
+        if(int.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не число");
+        }
+    }
+
+    return result;
+}
+
+int[,] InitMatrixTask48(int rows, int columns)
+{
+    int[,] matrix = new int[rows, columns];
+    
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i,j] = i + j;
+        }
+    } 
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+
+    Console.WriteLine();
+}
+
+int rows = GetNumber("Введите количество строк:");
+int columns = GetNumber("Введите количество столбцов:");
+int[,] matrix = InitMatrixTask48(rows, columns);
+PrintMatrix(matrix);
