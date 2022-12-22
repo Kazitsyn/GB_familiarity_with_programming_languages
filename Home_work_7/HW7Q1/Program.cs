a8d4eaece83d98273b857aab9342548f7d72236a
Kazitsyn <71144819+Kazitsyn@users.noreply.github.com>
2022-12-22 19:45:13 +0300

    add hw7

---
+++ Home_work_7/HW7Q1/Program.cs
@@ -0,0 +1,41 @@
+﻿/* 
+Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+m = 3, n = 4.
+0,5 7 -2 -0,2
+1 -3,3 8 -9,9
+8 7,8 -7,1 9
+*/
+
+double[ , ] GetRandomArray(int line, int column)
+{
+    double[ , ] array = new double[line, column];
+    Random rnd = new Random();
+    for (int i = 0; i < line; i++)
+    {
+        for (int j = 0; j < column; j++)
+        {
+            array[i,j] = rnd.NextDouble() * 10;
+        }
+    }
+    return array;
+}
+
+void PrintArray(double[ , ] array)
+{
+    Console.WriteLine($"m = {array.GetLength(0)}, n = {array.GetLength(1)}.");
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write("{0:F1}", array[i, j]);
+            Console.Write(" ");
+        }
+            
+        Console.WriteLine();
+    }
+    
+}
+
+double[ , ] array = GetRandomArray(3,4);
+PrintArray(array);
---
+++ Home_work_7/HW7Q2/Program.cs
@@ -0,0 +1,51 @@
+﻿/*
+Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+17 -> такого числа в массиве нет
+*/
+
+
+int[,] matrix = new int[,]{ {1, 4, 7, 2},
+                            {5, 9, 2, 3},
+                            {8, 4, 2, 4}};
+
+
+
+string GetElementMatrix(int[,] matrix, int i, int j)
+{
+    string result = String.Empty;
+    if (i < matrix.GetLength(0) && j < matrix.GetLength(1)) return Convert.ToString(matrix[i,j]);
+    else return "такого числа в массиве нет";
+    
+}
+
+int GetNumber(string message)
+{
+    int result = 0;
+
+    while(true)
+    {
+        Console.Write (message);
+
+        if(int.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не число");
+        }
+    }
+
+    return result;
+}
+
+int i = GetNumber("i = ");
+int j = GetNumber("j = ");
+string element = GetElementMatrix(matrix, i, j);
+Console.WriteLine($"{i} {j} -> {element}");
