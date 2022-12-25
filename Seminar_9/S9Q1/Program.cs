a242710b0aeadc127eb74f5678ef2536906d3e8f
Kazitsyn <71144819+Kazitsyn@users.noreply.github.com>
2022-12-25 21:05:54 +0300

    add s9

---
+++ Seminar_9/S9Q1/Program.cs
@@ -0,0 +1,33 @@
+﻿/*
+Задача 63: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
+N = 5 -> "1, 2, 3, 4, 5"
+N = 6 -> "1, 2, 3, 4, 5, 6"
+*/
+
+string NumbersRec(int num, int N)
+{
+    if (num <= N) return $"{num} "+ NumbersRec(num + 1, N);
+    else return String.Empty;
+}
+
+
+Console.WriteLine(NumbersRec(5,10));
+
+/*
+Console.Write("Введите N: ");
+var str = Console.ReadLine() ?? "";
+
+int N = int.Parse(str);
+
+string AddN(int n)
+{
+    if (n == 1) 
+        return n.ToString();
+    else
+    {
+        return AddN(n-1) + ", " + n;
+    }
+}
+
+Console.WriteLine(AddN(N));
+*/
---
+++ Seminar_9/S9Q2/Program.cs
@@ -0,0 +1,14 @@
+﻿/*
+Задача 65: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+M = 1; N = 5 -> "1, 2, 3, 4, 5"
+M = 4; N = 8 -> "4, 6, 7, 8"
+*/
+
+string NumbersRec(int num, int N)
+{
+    if (num <= N) return $"{num} "+ NumbersRec(num + 1, N);
+    else return String.Empty;
+}
+
+
+Console.WriteLine(NumbersRec(5,10));
